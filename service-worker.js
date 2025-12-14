30b118b8ba9845e9cef8a26aed71917dc4d0ca43
GugaToca <gustavotocalino08@outlook.com>
2025-12-14 11:41:58 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "diario-cargas-v5"; // 👈 MUDE A VERSÃO SEMPRE QUE ATUALIZAR
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./cadastro.html",
+  "./styles.css",
+  "./app.js",
+  "./auth.js",
+  "./firebase-config.js",
+  "./imagem_logistica.png"
+];
+
+// Instala o novo SW
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // força ativação imediata
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Ativa e limpa caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim(); // aplica em todas as abas
+});
+
+// Busca
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
